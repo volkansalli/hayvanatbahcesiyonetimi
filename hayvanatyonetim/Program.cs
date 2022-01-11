ea524b194dd8ae5f3e6c7d1fdc13dfabe1215594
volkan_salli <volkan_salli@hotmail.com>
2022-01-12 00:22:09 +0300

    Add files via upload

---
+++ hayvanatyonetim/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+namespace hayvanatyonetim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            atlar at=new();
+            at.getFeedSchedule();
+
+        }
+    }
+    public abstract class hayvanlar
+    {
+        public string yas;
+        public string agirlik;
+        public virtual void getDosage()
+        {
+
+        }
+        public virtual void getFeedSchedule()
+        {
+
+        }
+    }
+    public class atlar:hayvanlar
+    {
+        public string tur="atlar";
+        public override void getDosage()
+        {
+            Console.WriteLine("haftada 1 doz");
+        }
+        public override void getFeedSchedule()
+        {
+            Console.WriteLine("günde 2 balya saman");
+        }
+    }
+    public class kedigillerlar:hayvanlar
+    {
+        public string tur="kedigiller";
+        public override void getDosage()
+        {
+            Console.WriteLine("haftada 2 doz");
+        }
+        public override void getFeedSchedule()
+        {
+            Console.WriteLine("günde 5 kg kemikli et");
+        }
+    }
+    public class kemirgenler:hayvanlar
+    {
+        public string tur="kemirgenler";
+        public override void getDosage()
+        {
+            Console.WriteLine("haftada 3 doz");
+        }
+        public override void getFeedSchedule()
+        {
+            Console.WriteLine("günde 1 kg bugday");
+        }
+    }
+}
